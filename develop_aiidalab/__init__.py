9497ec1370b4ad282947ecdefed79fb2c6bff5c9
Carl Simon Adorf <simon.adorf@epfl.ch>
2021-10-06 14:31:27 +0000

    Update setup flow to use setup.cfg file.
     - And update pre-commit configuration.
     - And remove obsolete 'app' command.

--- develop_aiidalab/__init__.py
+++ develop_aiidalab/__init__.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Utility CLI to aid in AiiDAlab development."""
+import json
+
 # pylint: disable=invalid-name,too-many-branches
 import logging
-import json
 from pathlib import Path
 from subprocess import run
 
-import click
-import dulwich
-import toml
-from tabulate import tabulate
-
 import aiidalab.config
-from aiidalab.app import AiidaLabApp
-from aiidalab.utils import load_app_registry_entry
-from aiidalab.utils import load_app_registry_index
-
+import click
+import toml
+from aiidalab.utils import load_app_registry_entry, load_app_registry_index
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -24,33 +19,8 @@ logging.basicConfig(level=logging.DEBUG)
 _FMT_BOOL_AND_NONE = {None: "n/a", False: "no", True: "yes"}
 
 
-
-def _get_app_from_name(name):
-    """Return the app instance from name."""
-    apps = load_app_registry()['apps']
-    app_data = apps.get(name)
-    if app_data is None:
-        click.secho(f"Did not find app '{name}' in app registry.", fg='yellow')
-    return AiidaLabApp(name, app_data, aiidalab.config.AIIDALAB_APPS, watch=False)
-
-
-def _get_app():
-    """Return the app instance from the current working directory."""
-    try:
-        name = str(Path.cwd().relative_to(aiidalab.config.AIIDALAB_APPS))
-        if name == '.':
-            raise ValueError
-    except ValueError:
-        raise click.ClickException(f"The current directory is not a sub-directory of '{aiidalab.config.AIIDALAB_APPS}'.")
-
-    try:
-        return _get_app_from_name(name)
-    except dulwich.errors.NotGitRepository:
-        raise click.ClickException("The app directory must be a git repository.")
-
-
 @click.group()
-@click.option('--local-prefix', type=click.Path(), default='~/local')
+@click.option("--local-prefix", type=click.Path(), default="~/local")
 @click.pass_context
 def cli(ctx, local_prefix):
     """Manage the AiiDAlab development environment."""
@@ -62,33 +32,36 @@ def cli(ctx, local_prefix):
     class Paths:  # noqa
         """Internal class to pass computed paths sub-commands."""
 
-        home_app_system = Path('/opt/aiidalab-home')
-        home_app_user = apps_path.joinpath('home')
+        home_app_system = Path("/opt/aiidalab-home")
+        home_app_user = apps_path.joinpath("home")
 
-        aiidalab_dev = local_prefix.joinpath('aiidalab')
-        aiidalab_dev_package = aiidalab_dev.joinpath('aiidalab')
+        aiidalab_dev = local_prefix.joinpath("aiidalab")
+        aiidalab_dev_package = aiidalab_dev.joinpath("aiidalab")
 
-        aiidalab_home_dev = local_prefix.joinpath('aiidalab-home')
-        aiidalab_home_dev_aiidalab_link = aiidalab_home_dev.joinpath('aiidalab')
+        aiidalab_home_dev = local_prefix.joinpath("aiidalab-home")
+        aiidalab_home_dev_aiidalab_link = aiidalab_home_dev.joinpath("aiidalab")
 
-        home_app_system = Path('/opt/aiidalab-home')
-        home_app_user = apps_path.joinpath('home')
+        home_app_system = Path("/opt/aiidalab-home")
+        home_app_user = apps_path.joinpath("home")
 
-        config_file = Path.home() / 'aiidalab.toml'
+        config_file = Path.home() / "aiidalab.toml"
 
-    ctx.obj['paths'] = Paths
+    ctx.obj["paths"] = Paths
 
 
 @cli.command()
 def config():
     """Show the current configuration."""
-    cfg = {key[len('AIIDALAB_'):].lower() : getattr(aiidalab.config, key)
-           for key in dir(aiidalab.config) if key.startswith('AIIDALAB_')}
+    cfg = {
+        key[len("AIIDALAB_") :].lower(): getattr(aiidalab.config, key)
+        for key in dir(aiidalab.config)
+        if key.startswith("AIIDALAB_")
+    }
     click.echo(toml.dumps(cfg))
 
 
 @cli.command()
-@click.option('-a', '--app')
+@click.option("-a", "--app")
 def registry(app):
     """Show the registry."""
     if app:
@@ -101,7 +74,7 @@ def registry(app):
 @click.pass_context
 def restore(ctx):
     """Restore the system configuration of the home app."""
-    paths = ctx.obj['paths']
+    paths = ctx.obj["paths"]
 
     assert paths.home_app_system.exists()
 
@@ -112,43 +85,47 @@ def restore(ctx):
                 paths.home_app_user.unlink()
                 paths.home_app_user.symlink_to(paths.home_app_system)
             elif paths.home_app_user.resolve() != paths.home_app_system.resolve():
-                raise click.ClickException(f"Link {paths.home_app_user} exists and points to unexpected location.")
+                raise click.ClickException(
+                    f"Link {paths.home_app_user} exists and points to unexpected location."
+                )
         else:
-            raise click.ClickException(f"{paths.home_app_user} exists and is not expected symbolic link.")
+            raise click.ClickException(
+                f"{paths.home_app_user} exists and is not expected symbolic link."
+            )
     else:
         click.echo(f"Link {paths.home_app_user} -> {paths.home_app_system}")
         paths.home_app_user.symlink_to(paths.home_app_system)
 
     try:
         config = toml.loads(paths.config_file.read_text())
-        config['develop'] = False
+        config["develop"] = False
         paths.config_file.write_text(toml.dumps(config))
     except FileNotFoundError:
         pass  # config file does not exist, nothing to do
 
-    click.secho("Mode: SYSTEM", fg='green')
+    click.secho("Mode: SYSTEM", fg="green")
 
 
 @cli.command()
 @click.pass_context
 def status(ctx):
     """Show current status of the AiiDAlab environment."""
-    paths = ctx.obj['paths']
+    paths = ctx.obj["paths"]
 
     issues = False
 
     def msg_issue(msg):
         nonlocal issues
         issues = True
-        click.secho(f"\u2718 {msg}", fg='red')
+        click.secho(f"\u2718 {msg}", fg="red")
 
     def msg_ok(msg):
-        click.secho(f"\u2714 {msg}", fg='green')
+        click.secho(f"\u2714 {msg}", fg="green")
 
     dev_mode = paths.home_app_user.resolve() == paths.aiidalab_home_dev.resolve()
     system_mode = paths.home_app_user.resolve() == paths.home_app_system.resolve()
 
-    mode = 'DEVELOPMENT' if dev_mode else ('SYSTEM' if system_mode else 'UNKNOWN')
+    mode = "DEVELOPMENT" if dev_mode else ("SYSTEM" if system_mode else "UNKNOWN")
     click.secho(f"Mode: {mode}")
 
     if dev_mode:
@@ -167,20 +144,29 @@ def status(ctx):
         except FileNotFoundError:
             msg_issue(f"File '{paths.config_file!s}' does not exist.")
         else:
-            if config.get('develop'):
+            if config.get("develop"):
                 msg_ok(f"Key 'develop' set to True in '{paths.config_file!s}'.")
             else:
                 msg_issue(f"Key 'develop' not set to True in '{paths.config_file!s}'.")
 
-        if paths.aiidalab_home_dev_aiidalab_link.resolve() == paths.aiidalab_dev_package.resolve():
-            msg_ok(f"Link {paths.aiidalab_home_dev_aiidalab_link} to {paths.aiidalab_dev_package} is set.")
+        if (
+            paths.aiidalab_home_dev_aiidalab_link.resolve()
+            == paths.aiidalab_dev_package.resolve()
+        ):
+            msg_ok(
+                f"Link {paths.aiidalab_home_dev_aiidalab_link} to {paths.aiidalab_dev_package} is set."
+            )
         else:
-            msg_issue(f"Link {paths.aiidalab_home_dev_aiidalab_link} to {paths.aiidalab_dev_package} is missing.")
+            msg_issue(
+                f"Link {paths.aiidalab_home_dev_aiidalab_link} to {paths.aiidalab_dev_package} is missing."
+            )
 
         if paths.home_app_user.resolve() == paths.aiidalab_home_dev.resolve():
             msg_ok(f"Link {paths.home_app_user} to {paths.aiidalab_home_dev} is set.")
         else:
-            msg_issue(f"Link {paths.home_app_user} to {paths.aiidalab_home_dev} is missing.")
+            msg_issue(
+                f"Link {paths.home_app_user} to {paths.aiidalab_home_dev} is missing."
+            )
     elif system_mode:
         msg_ok(f"Link {paths.home_app_user} to {paths.home_app_system} is set.")
 
@@ -189,13 +175,15 @@ def status(ctx):
         except FileNotFoundError:
             msg_ok(f"Local config file '{paths.config_file!s}' does not exist.")
         else:
-            if config.get('develop'):
+            if config.get("develop"):
                 msg_issue(f"Key 'develop' set to True in '{paths.config_file!s}'.")
             else:
                 msg_ok(f"Key 'develop' not set to True in '{paths.config_file!s}'.")
 
     elif paths.home_app_user.exists():
-        msg_issue("The home app is installed, but is either a local directory or points to an unknown location.")
+        msg_issue(
+            "The home app is installed, but is either a local directory or points to an unknown location."
+        )
 
     else:
         msg_issue("The home app is not installed.")
@@ -206,75 +194,82 @@ def status(ctx):
 
 @cli.command()
 @click.pass_context
-@click.option('-u', '--github-username', default='aiidalab')
-@click.option('--use-ssh', is_flag=True)
+@click.option("-u", "--github-username", default="aiidalab")
+@click.option("--use-ssh", is_flag=True)
 def setup(ctx, github_username, use_ssh):
     """Set up the AiiDAlab environment for development."""
-    paths = ctx.obj['paths']
+    paths = ctx.obj["paths"]
 
-    aiidalab_url = f"git@github.com:{github_username}/aiidalab.git" if use_ssh else \
-            f"https://github.com/{github_username}/aiidalab.git"
-    aiidalab_home_url = f"git@github.com:{github_username}/aiidalab-home.git" if use_ssh else \
-            f"https://github.com/{github_username}/aiidalab-home.git"
+    aiidalab_url = (
+        f"git@github.com:{github_username}/aiidalab.git"
+        if use_ssh
+        else f"https://github.com/{github_username}/aiidalab.git"
+    )
+    aiidalab_home_url = (
+        f"git@github.com:{github_username}/aiidalab-home.git"
+        if use_ssh
+        else f"https://github.com/{github_username}/aiidalab-home.git"
+    )
 
     # Setup aiidalab package development directory.
     if not paths.aiidalab_dev.exists():
-        run(['git', 'clone', aiidalab_url, paths.aiidalab_dev], check=True)
+        run(["git", "clone", aiidalab_url, paths.aiidalab_dev], check=True)
 
     # Setup aiidalab-home development directory.
     if not paths.aiidalab_home_dev.exists():
-        run(['git', 'clone', aiidalab_home_url, paths.aiidalab_home_dev], check=True)
+        run(["git", "clone", aiidalab_home_url, paths.aiidalab_home_dev], check=True)
 
     # Link aiidalab development package into aiidalab-home development directory.
     if paths.aiidalab_home_dev_aiidalab_link.exists():
-        if paths.aiidalab_home_dev_aiidalab_link.resolve() != paths.aiidalab_dev_package.resolve():
+        if (
+            paths.aiidalab_home_dev_aiidalab_link.resolve()
+            != paths.aiidalab_dev_package.resolve()
+        ):
             raise click.ClickException(
-                f"Link {paths.aiidalab_home_dev_aiidalab_link} already exists and points to unknown location.")
+                f"Link {paths.aiidalab_home_dev_aiidalab_link} already exists and points to unknown location."
+            )
     else:
-        click.echo(f"Link {paths.aiidalab_home_dev_aiidalab_link} -> {paths.aiidalab_dev_package}")
-        paths.aiidalab_home_dev_aiidalab_link.symlink_to(paths.aiidalab_dev_package, target_is_directory=True)
+        click.echo(
+            f"Link {paths.aiidalab_home_dev_aiidalab_link} -> {paths.aiidalab_dev_package}"
+        )
+        paths.aiidalab_home_dev_aiidalab_link.symlink_to(
+            paths.aiidalab_dev_package, target_is_directory=True
+        )
 
     # Link aiidalab-home development development directory into apps directory.
     if paths.home_app_user.exists():
         if paths.home_app_user.is_symlink():
             if paths.home_app_user.resolve() == paths.home_app_system.resolve():
-                click.echo(f'Unlink {paths.home_app_user}')
+                click.echo(f"Unlink {paths.home_app_user}")
                 paths.home_app_user.unlink()
                 click.echo(f"Link {paths.home_app_user} -> {paths.aiidalab_home_dev}")
-                paths.home_app_user.symlink_to(paths.aiidalab_home_dev, target_is_directory=True)
+                paths.home_app_user.symlink_to(
+                    paths.aiidalab_home_dev, target_is_directory=True
+                )
             elif paths.home_app_user.resolve() != paths.aiidalab_home_dev.resolve():
-                raise click.ClickException(f"Link {paths.home_app_user} already exists and points at unknown location.")
+                raise click.ClickException(
+                    f"Link {paths.home_app_user} already exists and points at unknown location."
+                )
         elif paths.home_app_user.is_dir():
             raise click.ClickException(
-                f"Unable to setup home app for development, exxisting directory at {paths.home_app_user}.")
+                f"Unable to setup home app for development, exxisting directory at {paths.home_app_user}."
+            )
     else:
         click.echo(f"Link {paths.home_app_user} -> {paths.aiidalab_home_dev}")
-        paths.home_app_user.symlink_to(paths.aiidalab_home_dev, target_is_directory=True)
+        paths.home_app_user.symlink_to(
+            paths.aiidalab_home_dev, target_is_directory=True
+        )
 
     # Set 'develop' key in local aiidalab config.
     try:
         config = toml.loads(paths.config_file.read_text())
     except FileNotFoundError:
         config = dict()
-    config['develop'] = True
+    config["develop"] = True
     paths.config_file.write_text(toml.dumps(config))
 
-    click.secho("Mode: DEVELOPMENT", fg='green')
+    click.secho("Mode: DEVELOPMENT", fg="green")
 
 
-@cli.command()
-def app():
-    """Show basic information about an app and the installation status."""
-    app = _get_app()
-    rows = list()
-    rows.append(("AiiDAlab app", app.name))
-    rows.append(("Version:", app.installed_version))
-    rows.append(("Detached:", _FMT_BOOL_AND_NONE[app.detached]))
-    rows.append(("Compatible:", _FMT_BOOL_AND_NONE[app.compatible]))
-
-    click.echo(tabulate(rows))
-
-
-
-if __name__ == '__main__':
-    cli(auto_envvar_prefix='AIIDALAB_DEVELOP')  # noqa
+if __name__ == "__main__":
+    cli(auto_envvar_prefix="AIIDALAB_DEVELOP")  # noqa
--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 # -*- coding: utf8 -*-
-"""Setting up base widgets for base package for AiiDA lab."""
-import json
-
+"""This file is required for editable installs of the package."""
 from setuptools import setup
 
-with open('setup.json', 'r') as info:
-    kwargs = json.load(info)  # pylint: disable=invalid-name
-
-with open('requirements.txt', 'r') as rfile:
-    requirements = rfile.read().splitlines()  # pylint: disable=invalid-name
-
-# -i https://pypi.org/simple not supported in install_requires
-if requirements[0].startswith('-i'):
-    requirements.pop(0)
-
-setup(long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      install_requires=requirements,
-      entry_points={'console_scripts': ['develop-aiidalab = develop_aiidalab:cli']},
-      **kwargs)
+setup()
